6defcee11cacb50f91e31c4317cb4e3ddfc57f70
Nazim Boudeffa <boudeffa@gmail.com>
2022-05-10 00:08:23 +0200

    Updated the example

--- examples/product.js
+++ examples/product.js
@@ -1,11 +1,11 @@
 const kinguin = require('../index.js');
 
-var k = new kinguin();
+var king = new kinguin();
 
 //Trying with CyberPunk2077
 
-//k.getProductByKinguinId('64435');
+//king.getProductByKinguinId('64435');
 
-//k.getProductBySteamId('1091500')
+//king.getProductBySteamId('1091500')
 
-k.getProductByName('Cyberpunk 2077');
+king.getProductByName('Cyberpunk 2077');
